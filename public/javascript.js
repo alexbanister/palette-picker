e61ba08d5b3788de714dd981012ea007cc73f013
Alex Banister <alexbanister@gmail.com>
2017-11-27 20:58:54 -0700

    add styles

--- public/javascript.js
+++ public/javascript.js
@@ -1,1 +1,19 @@
-console.log('it works');
+const generateColor = () => {
+  const red = generateColorValue();
+  const green = generateColorValue();
+  const blue = generateColorValue();
+  return `#${red}${green}${blue}`;
+};
+
+const generateColorValue = () => {
+  const value = Math.floor(Math.random() * 255).toString(16);
+  return value.length === 1 ? "0" + value : value;
+};
+
+$('h1').on('click', () => {
+  $('h1').text('test');
+});
+console.log(generateColor());
+console.log(generateColor());
+console.log(generateColor());
+console.log(generateColor());
