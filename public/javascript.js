8e9251dbebf66996f58e5b00d0d6ea8c3d6aac3a
Alex Banister <alexbanister@gmail.com>
2017-11-28 12:16:15 -0700

    add airbnb linter

--- public/javascript.js
+++ public/javascript.js
@@ -10,40 +10,40 @@ const generateColor = () => {
 
 const generateColorValue = () => {
   const value = Math.floor(Math.random() * 255).toString(16);
-  return value.length === 1 ? "0" + value : value;
+  return value.length === 1 ? '0' + value : value;
 };
 
 const setOneColor = (color, position) => {
   $('.color'+position).css('background', color);
   $('.color'+position).css('color', color);
   $('.color-letter').css('background', 'none');
-  $('[data-id='+position+']').find("h3").text(color);
-}
+  $('[data-id='+position+']').find('h3').text(color);
+};
 
 const shuffleColors = () => {
   for (var i = 0; i < 5; i++) {
     const locked = lockedPositions.find( pos => pos === i+1);
-    const color = generateColor();
+    let color = generateColor();
     while (currentColors.includes(color)) {
       color = generateColor();
     }
     if (!locked) {
-      setOneColor(color, i+1)
+      setOneColor(color, i+1);
     }
   }
-}
+};
 
 const toggleLocked = (e) => {
-  const position = $(e.target).closest("[data-id]").data("id");
+  const position = $(e.target).closest('[data-id]').data('id');
   const locked = lockedPositions.find( pos => pos === position);
   if (locked) {
     lockedPositions = lockedPositions.filter( pos => pos !== position);
-    $('[data-id='+position+']').find("img").attr('src', 'images/keep.svg');
+    $('[data-id='+position+']').find('img').attr('src', 'images/keep.svg');
   } else {
     lockedPositions = [...lockedPositions, position];
-    $('[data-id='+position+']').find("img").attr('src', 'images/locked.svg');
+    $('[data-id='+position+']').find('img').attr('src', 'images/locked.svg');
   }
-}
+};
 
 $('[name="shuffle-colors"]').on('click', shuffleColors);
 $('.lock').on('click', toggleLocked);
