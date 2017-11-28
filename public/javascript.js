d192e546791942bb7a190799ae64aee7ce6fb62a
Alex Banister <alexbanister@gmail.com>
2017-11-27 21:42:55 -0700

    add random color generator and color position lock functionality

--- public/javascript.js
+++ public/javascript.js
@@ -1,3 +1,6 @@
+var lockedPositions = [];
+var currentColors = [];
+
 const generateColor = () => {
   const red = generateColorValue();
   const green = generateColorValue();
@@ -10,10 +13,37 @@ const generateColorValue = () => {
   return value.length === 1 ? "0" + value : value;
 };
 
-$('h1').on('click', () => {
-  $('h1').text('test');
-});
-console.log(generateColor());
-console.log(generateColor());
-console.log(generateColor());
-console.log(generateColor());
+const setOneColor = (color, position) => {
+  $('.color'+position).css('background', color);
+  $('.color'+position).css('color', color);
+  $('.color-letter').css('background', 'none');
+  $('[data-id='+position+']').find("h3").text(color);
+}
+
+const shuffleColors = () => {
+  for (var i = 0; i < 5; i++) {
+    const locked = lockedPositions.find( pos => pos === i+1);
+    const color = generateColor();
+    while (currentColors.includes(color)) {
+      color = generateColor();
+    }
+    if (!locked) {
+      setOneColor(color, i+1)
+    }
+  }
+}
+
+const toggleLocked = (e) => {
+  const position = $(e.target).closest("[data-id]").data("id");
+  const locked = lockedPositions.find( pos => pos === position);
+  if (locked) {
+    lockedPositions = lockedPositions.filter( pos => pos !== position);
+    $('[data-id='+position+']').find("img").attr('src', 'images/keep.svg');
+  } else {
+    lockedPositions = [...lockedPositions, position];
+    $('[data-id='+position+']').find("img").attr('src', 'images/locked.svg');
+  }
+}
+
+  $('[name="shuffle-colors"]').on('click', shuffleColors);
+  $('.lock').on('click', toggleLocked);
