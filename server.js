f6f5844ae08ba6cc715ab9502082a2a99ea67e7f
Alex Banister <alexbanister@gmail.com>
2017-11-29 11:39:14 -0700

    add projects and palettes post endpoints to db

--- server.js
+++ server.js
@@ -53,6 +53,26 @@ app.get('/api/v1/projects/:projectId/palettes', (request, response) => {
       response.status(500).json({ error });
     });
 });
+app.post('/api/v1/projects/:projectId/palettes', (request, response) => {
+  const palette = Object.assign({}, request.body, { project_id: request.params.projectId });
+
+  for (let requiredParameter of ['name', 'color1', 'color2', 'color3', 'color4', 'color5']) {
+    if (!palette[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('palettes').insert(palette, 'id')
+    .then(palette => {
+      response.status(201).json({ id: palette[0] });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
